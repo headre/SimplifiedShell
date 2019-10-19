640f1d36522f25e9280a58c14c55095851d10fa7
headre <45737374+headre@users.noreply.github.com>
2019-10-19 21:45:56 +0800

    help and clear added

---
+++ Shell_v1.5/Parse.c
@@ -0,0 +1,81 @@
+#include "Shell.h"
+
+command parse(char *commandinput)
+{
+    command template;
+    char *Piece,*PiecePart[LIST_SIZE],blank[6]=" \n\0";
+    template.sequence=0;
+    template = initialize(template);
+    for(int i=0;i<LIST_SIZE;i++){
+        PiecePart[i]=NULL;
+    }
+    int i=1;
+    Piece = strtok(commandinput,blank);
+    PiecePart[0]=Piece;
+    while (Piece !=  NULL)
+    {
+        Piece=strtok(NULL,blank);
+        if(Piece!=NULL){
+            PiecePart[i]=Piece;
+        }
+        i++;
+    }
+    i=0;
+    while (PiecePart[i]!=NULL) {
+        template.args[i]=(char *)malloc(sizeof(char)*MAX_SIZE);
+        strcpy(template.args[i], PiecePart[i]);
+        if(strcmp(template.args[i], "|")==0){
+            template.sequence=10+i;
+            if(i<2){
+                template.sequence=-1;
+                break;
+            }
+        }
+        if(strcmp(template.args[i],">")==0){
+            template.sequence=20+i;
+            if(i<2){
+                template.sequence=-1;
+                break;
+            }
+        }
+        i++;
+    }
+    if(template.sequence<10&&template.sequence>0){
+        if(strcmp(PiecePart[0],"exit")==0){
+            template.sequence=0;
+        }
+        else if(strcmp(PiecePart[0],"info")==0){
+            template.sequence=1;
+        }
+        else if(strcmp(PiecePart[0],"pwd")==0){
+            template.sequence=2;
+        }
+        else if(strcmp(PiecePart[0],"cd")==0){
+            template.sequence=3;
+        }
+        else if(strcmp(PiecePart[0],"ex")==0&&PiecePart[1]!=NULL){
+            template.sequence=4;
+        }
+        else if(strcmp(PiecePart[0],"mygrep")==0&&PiecePart[3]!=NULL&&PiecePart[2]!=NULL&&strcmp(PiecePart[1],"-c")==0){
+            template.sequence=5;
+        }
+        else if(strcmp(PiecePart[0],"clear")==0){
+            template.sequence=6;
+        }
+        else if(strcmp(PiecePart[0],"help")==0){
+            template.sequence=7;
+        }else
+        {
+            template.sequence=-1;
+        }
+        
+    }
+    return template;
+};
+command initialize(command orig){
+    orig.sequence=0;
+    for(int i=0;i<LIST_SIZE;i++){
+        orig.args[i]=NULL;
+    }
+    return orig;
+}
